dad7544f64031a2d025b216ab5c2bf3c1983dc28
laitrungdung2000 <laidung29022000@gmail.com>
2020-08-21 11:14:49 +0700

    add pub kafka change thread

--- webstreaming.py
+++ webstreaming.py
@@ -9,18 +9,21 @@ from flask import Flask
 from flask import render_template
 import threading
 import argparse
-import datetime
 import imutils
 import time
 import cv2
 import redis
 import xml.etree.ElementTree as ET
-
+from kafka import KafkaProducer
+import json
+from datetime import datetime, timedelta
+import decimal
 # initialize the output frame and a lock used to ensure thread-safe
 # exchanges of the output frames (useful for multiple browsers/tabs
 # are viewing tthe stream)
 
 env = 'camera'
+topic = 'numtest'
 outputFrame = None
 lock = threading.Lock()
 
@@ -28,7 +31,7 @@ client = redis.Redis(host='localhost', port=6379, db=0)
 
 # initialize a flask object
 app = Flask(__name__)
-udp = 'udp://127.0.0.1:5005'
+udp = 'udp://177.0.0.1:5005'
 
 send_data = {
 	"link_udp": udp,
@@ -36,21 +39,28 @@ send_data = {
     "time": ''
 }
 
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
 # initialize the video stream and allow the camera sensor to
 # warmup
 #vs = VideoStream(usePiCamera=1).start()
 vs = VideoStream(src=udp).start()
 time.sleep(2.0)
 
+start_time = ''
+end_time = 0
 @app.route("/")
 def index():
 	# return the rendered template
 	return render_template("index.html")
 
+
 def detect_motion(frameCount):
 	# grab global references to the video stream, output frame, and
 	# lock variables
-	global vs, outputFrame, lock
+	global vs, outputFrame, lock, start_time, end_time
 
 	# initialize the motion detector and the total number of frames
 	# read thus far
@@ -68,7 +78,7 @@ def detect_motion(frameCount):
 		gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
 		# grab the current timestamp and draw it on the frame
-		timestamp = datetime.datetime.now()
+		timestamp = datetime.now()
 		cv2.putText(frame, timestamp.strftime(
 			"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
@@ -77,12 +87,11 @@ def detect_motion(frameCount):
 		# number to construct a reasonable background model, then
 		# continue to process the frame
 		rect_list = []
-		data = ET.Element('data')
-		items = ET.SubElement(data, 'items')
+
+
 		if total > frameCount:
 			# detect motion in the image
 			motion = md.detect(gray)
-
 			# cehck to see if motion was found in the frame
 			if motion is not None:
 				# unpack the tuple and draw the box surrounding the
@@ -90,31 +99,6 @@ def detect_motion(frameCount):
 				(thresh, (minX, minY, maxX, maxY)) = motion
 				cv2.rectangle(frame, (minX, minY), (maxX, maxY),
 					(0, 0, 255), 2)
-				file = file + 1
-				item = ET.SubElement(items, 'item')
-				item.set('name', 'item')
-				point1 = ET.SubElement(item, "point")
-				point2 = ET.SubElement(item, "point")
-
-				# point 1
-				point1.set('name', 'point1')
-				x1 = ET.SubElement(point1, 'x')
-				y1 = ET.SubElement(point1, 'y')
-				x1.text = str(minX)
-				y1.text = str(minY)
-
-				# point 2
-				point2.set('name', 'point2')
-				x2 = ET.SubElement(point2, 'x')
-				y2 = ET.SubElement(point2, 'y')
-				x2.text = str(maxX)
-				y2.text = str(maxY)
-
-				mydata = ET.tostring(data).decode('utf-8')
-				# now = datetime.now()
-				# dt_string = now.strftime("%d/%m/%Y%H:%M:%S.xml")
-				myfile = open('xml/' + str(file) + '.xml', "w")
-				myfile.write(mydata)
 
 				item = {
 					"point1": {
@@ -129,7 +113,52 @@ def detect_motion(frameCount):
 				rect_list.append(item)
 				send_data['rect_list'] = rect_list
 				client.publish(env, str(send_data))
-				cv2.imwrite('images/' + str(file) + '.jpg', frame)
+				producer.send(topic, value=send_data)
+				if start_time == '':
+					start_time = datetime.now()
+					client.publish(env, 'Start_time:' + str(start_time))
+					data = ET.Element('data')
+					items = ET.SubElement(data, 'items')
+					file = file + 1
+					item = ET.SubElement(items, 'item')
+					item.set('name', 'item')
+					point1 = ET.SubElement(item, "point")
+					point2 = ET.SubElement(item, "point")
+
+					# point 1
+					point1.set('name', 'point1')
+					x1 = ET.SubElement(point1, 'x')
+					y1 = ET.SubElement(point1, 'y')
+					x1.text = str(minX)
+					y1.text = str(minY)
+
+					# point 2
+					point2.set('name', 'point2')
+					x2 = ET.SubElement(point2, 'x')
+					y2 = ET.SubElement(point2, 'y')
+					x2.text = str(maxX)
+					y2.text = str(maxY)
+
+					mydata = ET.tostring(data).decode('utf-8')
+					# now = datetime.now()
+					# dt_string = now.strftime("%d/%m/%Y%H:%M:%S.xml")
+
+					myfile = open('xml/' + str(file) + '.xml', "w")
+					myfile.write(mydata)
+					cv2.imwrite('images/' + str(file) + '.jpg', frame)
+
+			if start_time != '':
+				if motion is None:
+					if end_time < 5:
+						t = datetime.now() - start_time
+						end_time = decimal.Decimal(t.seconds)
+					else:
+						client.publish(env, 'End time:' + str(datetime.now()))
+						end_time = 0
+						start_time = ''
+				else:
+					end_time = 0
+
 
 		# update the background model and increment the total number
 		# of frames read thus far
@@ -160,10 +189,10 @@ def generate():
 			# ensure the frame was successfully encoded
 			if not flag:
 				continue
-				
+
 
 		# yield the output frame in the byte format
-		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
 			bytearray(encodedImage) + b'\r\n')
 
 @app.route("/video_feed")
