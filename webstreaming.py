9e20bf4bc1e9b83868080c955be3969f8fb055f5
laitrungdung2000 <laidung29022000@gmail.com>
2020-09-23 18:38:04 +0700

    clean webstreaming

--- webstreaming.py
+++ webstreaming.py
@@ -153,20 +153,6 @@ def on_connect():
                     cv2.rectangle(frame, (r['x'], r['y']), (r['x'] + r['w'], r['y'] + r['h']),
                                 (0, 0, 255), 2)
 
-                    # item = {
-                    #     "point1": {
-                    #         "x": r['x'],
-                    #         "y": r['y']
-                    #     },
-                    #     "point2": {
-                    #         "x": r['x'] + r['w'],
-                    #         "y": r['y'] + r['h']
-                    #     }
-                    # }
-                    # rect_list.append(item)
-                    # send_data['rect_list'] = rect_list
-                    # client.publish(str(send_data))
-                    # producer.producer(str(send_data))
                 if start_time == '':
                     start_time = datetime.now()
                     date_time = start_time.strftime("%m/%d/%Y_%H:%M:%S")
@@ -202,66 +188,8 @@ def on_connect():
         total += 1
         for item in pts:
             pts2 = np.array(item, np.int32)
-                #pts2 = pts2.reshape((-1, 1, 2))
-            frame = cv2.polylines(frame, [pts2], True, (0, 0, 255), 3)
-            rect_list = []
-            if total > 0:
-                # detect motion in the image
-                motion = md.detect(gray)
-                # cehck to see if motion was found in the frame
-                if motion is not None:
-                    # unpack the tuple and draw the box surrounding the
-                    # "motion area" on the output frame
-                    (thresh, rect) = motion
-                    for r in rect:
-                        cv2.rectangle(frame, (r['x'], r['y']), (r['x'] + r['w'], r['y'] + r['h']),
-                                    (0, 0, 255), 2)
-
-                        # item = {
-                        #     "point1": {
-                        #         "x": r['x'],
-                        #         "y": r['y']
-                        #     },
-                        #     "point2": {
-                        #         "x": r['x'] + r['w'],
-                        #         "y": r['y'] + r['h']
-                        #     }
-                        # }
-                        # rect_list.append(item)
-                    # send_data['rect_list'] = rect_list
-                    # # client.publish(str(send_data))
-                    # # producer.producer(str(send_data))
-                    # if start_time == '':
-                    #     start_time = datetime.now()
-                    #
-                    #     file = file + 1
-                    #
-                    #     xml = WriteXml(file)
-                    #     # xml.create_object(minX, minY, maxX, maxY)
-                    #
-                    #     t = WriteImage(file, frame)
-                    #     t.write_image()
-
-                # if start_time != '':
-                #     if motion is None:
-                #         if end_time < 5:
-                #             t = datetime.now() - start_time
-                #             end_time = decimal.Decimal(t.seconds)
-                #         else:
-                #             # client.publish('End time:' + str(datetime.now()))
-                #             end_time = 0
-                #             start_time = ''
-                #     else:
-                #         end_time = 0
-
-            # update the background model and increment the total number
-            # of frames read thus far
-            md.update(gray)
-            total += 1
-            for item in pts:
-                pts2 = np.array(item, np.int32)
-                #pts2 = pts2.reshape((-1, 1, 2))
-                frame = cv2.polylines(frame, [pts2], True, (0,0,255),3)
+            #pts2 = pts2.reshape((-1, 1, 2))
+            frame = cv2.polylines(frame, [pts2], True, (0,0,255),3)
         hello, frame = cv2.imencode('.jpg', frame)
 
         value = np.array(frame).tobytes()
